599e67e91ab5d01d3a1556d1bda93ff06cd9b4bb
devanshsharma <devansh.sharma@kloudspot.com>
2023-09-12 17:21:18 +0530

    Bubble Sort

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,5 +1,4 @@
 public class SelectionSort {
-
     public static void sort(int arr[]){
         for(int i=0;i<arr.length;i++){
             int min_index = i;
@@ -7,12 +6,10 @@ public class SelectionSort {
                 if(arr[min_index]>arr[j]){
                     min_index = j;
                 }
-
             }
             int temp = arr[min_index];
             arr[min_index] = arr[i];
             arr[i] = temp;
-
         }
         for(int i:arr){
             System.out.println(i);
